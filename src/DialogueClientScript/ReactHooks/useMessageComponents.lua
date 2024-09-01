3d45ace302d3fea011a96ca3fa260d1f14a4a9ad
Christian Toney <christian@makuwro.com>
2024-09-01 17:51:47 -0400

    Fix useMessageComponents

--- src/DialogueClientScript/ReactHooks/useMessageComponents.lua
+++ src/DialogueClientScript/ReactHooks/useMessageComponents.lua
@@ -61,58 +61,62 @@ local function useMessageComponents(props: useMessageComponentsProps)
 
       local page = pages[currentPageIndex];
 
-      -- Iterate through the page, run events, and show message text.
-      local dialogueContentItemIndex = #messageComponents;
-      if #page > dialogueContentItemIndex then
+      if page then
+        
+        -- Iterate through the page, run events, and show message text.
+        local dialogueContentItemIndex = #messageComponents + 1;
+        if #page > dialogueContentItemIndex then
 
-        local dialogueContentItem = page[dialogueContentItemIndex];
-        if dialogueContentItem.type == "effect" then
+          local dialogueContentItem = page[dialogueContentItemIndex];
+          if dialogueContentItem.type == "effect" then
 
-          print(`[Dialogue Maker] [{dialogueContentItemIndex}/{#page}] [Effect] {npcName or "Unknown NPC"}: {dialogueContentItem.name}`);
-          dialogueContentItem.run(isSkippingPage);
-          setMessageComponents({table.unpack(messageComponents), React.createElement(React.Fragment)});
+            print(`[Dialogue Maker] [{dialogueContentItemIndex}/{#page}] [Effect] {npcName or "Unknown NPC"}: {dialogueContentItem.name}`);
+            dialogueContentItem.run(isSkippingPage);
+            setMessageComponents({table.unpack(messageComponents), React.createElement(React.Fragment)});
 
-        elseif dialogueContentItem.type == "text" then
-          
-          -- Print to the debug console.
-          print(`[Dialogue Maker] [{dialogueContentItemIndex}/{#page}] [Message] {npcName or "Unknown NPC"}: {dialogueContentItem.text}`);
-          
-          -- Determine new offset.
-          setMessageComponents({table.unpack(messageComponents), React.createElement(TextSegment, {
-            text = dialogueContentItem.text;
-            skipPageEvent = skipPageEventRef.current;
-            layoutOrder = dialogueContentItemIndex;
-          })});
-
-        end;
-
-      else
-
-        -- Check if there are more pages.
-        if pages[currentPageIndex + 1] and isNPCTalking then
-
-          -- Wait for the player to click
-          props.setIsClickToContinueButtonVisible(true);
-
-        end;
-        setIsNPCTalking(false);
-        setIsSkippingPage(false);
-
-        -- Run the timeout code in the background
-        if npcSettings.timeout.enabled then
-
-          -- Wait for the player if the developer wants to
-          if #responseContentScripts == 0 or not npcSettings.timeout.waitForResponse then
-
-            local timeoutTask = task.delay(npcSettings.timeout.seconds, function()
+          elseif dialogueContentItem.type == "text" then
             
-              props.onTimeout();
+            -- Print to the debug console.
+            print(`[Dialogue Maker] [{dialogueContentItemIndex}/{#page}] [Message] {npcName or "Unknown NPC"}: {dialogueContentItem.text}`);
+            
+            -- Determine new offset.
+            setMessageComponents({table.unpack(messageComponents), React.createElement(TextSegment, {
+              text = dialogueContentItem.text;
+              skipPageEvent = skipPageEventRef.current;
+              layoutOrder = dialogueContentItemIndex;
+            })});
 
-            end);
+          end;
 
-            return function()
+        else
 
-              task.cancel(timeoutTask);
+          -- Check if there are more pages.
+          if pages[currentPageIndex + 1] and isNPCTalking then
+
+            -- Wait for the player to click
+            props.setIsClickToContinueButtonVisible(true);
+
+          end;
+          setIsNPCTalking(false);
+          setIsSkippingPage(false);
+
+          -- Run the timeout code in the background
+          if npcSettings.timeout.enabled then
+
+            -- Wait for the player if the developer wants to
+            if #responseContentScripts == 0 or not npcSettings.timeout.waitForResponse then
+
+              local timeoutTask = task.delay(npcSettings.timeout.seconds, function()
+              
+                props.onTimeout();
+
+              end);
+
+              return function()
+
+                task.cancel(timeoutTask);
+
+              end;
 
             end;
 
