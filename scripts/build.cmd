dc0d21d16650f763ca184f8d67ad3d4d1d557462
Christian Toney <christian.toney@beastslash.com>
2025-05-28 21:51:50 -0400

    update

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,20 +1,11 @@
 :: Install dependencies.
 pesde install
 
-:: Build StandardTheme.
-cd src\DialogueClientScript\src\Themes\StandardTheme
-rojo sourcemap -o sourcemap.json
-
 :: Build DialogueClientScript.
-cd ..\..\..
-rojo sourcemap -o sourcemap.json build.project.json
-darklua process src dist
+cd src\DialogueClientScript
+scripts\build.cmd
 
 :: Build DialoguePluginScript.
 cd ..\..
-rojo sourcemap -o sourcemap.json build.project.json
+rojo sourcemap -o sourcemap.json
 darklua process src dist
-
-:: Copy the build project files to the dist folder.
-copy src\DialogueClientScript\build.project.json dist\DialogueClientScript\default.project.json
-copy build.project.json dist\default.project.json
