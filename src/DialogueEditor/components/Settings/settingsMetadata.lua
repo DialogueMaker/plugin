ca6b0875309f71559ec43e1e0071e76c93901b77
Christian Toney <christian.toney@beastslash.com>
2025-06-17 16:40:12 -0400

    save for now

--- src/DialogueEditor/components/Settings/settingsMetadata.lua
+++ src/DialogueEditor/components/Settings/settingsMetadata.lua
@@ -1,10 +1,71 @@
 --!strict
 
 local root = script.Parent.Parent.Parent.Parent;
+local Client = require(root.DialogueMakerKit.Packages.Client);
 local Conversation = require(root.DialogueMakerKit.Packages.Conversation);
 local Dialogue = require(root.DialogueMakerKit.Packages.Dialogue);
 
+local function validateKey(key: string?): ()
+  
+  if not key then
+    
+    return;
+
+  end
+
+  local keyCode = Enum.KeyCode:FromName(key);
+
+  assert(keyCode, `Invalid key: {key}`);
+  
+end
+
 local metadata = {
+  loader = {
+    keybinds = {
+      interactKey = {
+        name = "Action key";
+        description = "The key that the player can press to interact with the dialogue.";
+        type = "string";
+        defaultValue = Client.defaultSettings.keybinds.interactKey.Name;
+        validator = validateKey;
+      };
+      interactKeyGamepad = {
+        name = "Gamepad action key";
+        description = "The key that the player can press on a gamepad to interact with the dialogue.";
+        type = "string";
+        defaultValue = Client.defaultSettings.keybinds.interactKeyGamepad.Name;
+        validator = validateKey;
+      };
+    };
+    theme = {
+      componentScript = {
+        name = "Component script";
+        description = "The script that contains the theme component.";
+        type = "Instance";
+        className = "ModuleScript";
+      };
+    };
+    typewriter = {
+      canPlayerSkipDelay = {
+        name = "Allow player to skip delay";
+        description = "If enabled, the player can skip the delay between characters being typed out in the conversation.";
+        type = "boolean";
+        defaultValue = Client.defaultSettings.typewriter.canPlayerSkipDelay;
+      };
+      characterDelaySeconds = {
+        name = "Character delay in seconds";
+        description = "The delay between each character being typed out in the conversation.";
+        type = "number";
+        defaultValue = Client.defaultSettings.typewriter.characterDelaySeconds;
+      };
+      shouldShowResponseWhileTyping = {
+        name = "Show response while typing";
+        description = "If enabled, responses will be shown while the typewriter effect is typing out the text.";
+        type = "boolean";
+        defaultValue = Client.defaultSettings.typewriter.shouldShowResponseWhileTyping;
+      };
+    };
+  };
   conversation = {
     speaker = {
       name = {
@@ -39,7 +100,7 @@ local metadata = {
         name = "Show response while typing";
         description = "If enabled, responses will be shown while the typewriter effect is typing out the text.";
         type = "boolean";
-        -- defaultValue = Conversation.defaultSettings.typewriter.shouldShowResponseWhileTyping;
+        defaultValue = Conversation.defaultSettings.typewriter.shouldShowResponseWhileTyping;
       };
     };
   };
@@ -79,17 +140,6 @@ local metadata = {
         type = "string";
         defaultValue = Dialogue.defaultSettings.speaker.name;
       }
-    };
-  };
-  loader = {
-    theme = {
-      componentScript = {
-        name = "Component script";
-        description = "The script that contains the theme component.";
-        type = "Instance";
-        className = "ModuleScript";
-        isRequired = true;
-      };
     };
   };
 };
